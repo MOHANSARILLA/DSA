1e9d65f96eecf39010a8d0945e716f9a01d985c3
mohansarilla <mohan.nitd2016@gmail.com>
2022-11-05 13:32:02 +0530

    UTILS: Added Helpers class to add util methods

---
+++ src/main/java/master/ds/utils/Helpers.java
@@ -0,0 +1,10 @@
+package master.ds.utils;
+
+public class Helpers {
+    public static void printArray(int[] array) {
+        for (int ele:array) {
+            System.out.print(" "+ele);
+        }
+        System.out.println();
+    }
+}
